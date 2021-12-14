dc3ecb9b8b00289048a2545bff6cdafb2b97016c
Kingq23 <80791284+Kingq23@users.noreply.github.com>
2021-12-14 14:14:03 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+function joke() {
+  // make an API request to https://icanhazdadjoke.com/'
+  fetch('https://icanhazdadjoke.com/', {
+    headers: {
+      'Accept': 'application/json'
+    }
+  }).then(function(response) {
+    /* convert Stringified JSON response to Javascript Object */
+    return response.json();
+  }).then(function(data) {
+    /* replace innerText of .joke-text with data.joke */
+    // extract the joke text
+    const joke = data.joke;
+    // do the replacement
+    document.getElementById("joke").innerHTML = joke
+
+});
+}
